6677a55c63823599d5846c9258c8379b39a6077a
Michael Peter <m.peter.dev@gmail.com>
2022-12-19 16:57:25 +0000

    help text on index.tsx. add devcontainer config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,27 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-full p-5 gap-2">
-      <p className="text-lg">This POC is integrated with SocialDB</p>
-      <Link href="widget/michaelpeter.near/Demo">
-        <span className="text-lg text-teal-400 underline">
-          /widget/michaelpeter.near/Demo
+    <div className="flex flex-col justify-center items-center h-full p-5 gap-12">
+      <p className="text-lg">
+        All widget code in this POC is fetched from SocialDB
+      </p>
+      <div className="flex flex-col gap-1 items-center">
+        <Link href="widget/michaelpeter.near/Demo">
+          <span className="text-lg text-teal-400 underline">
+            /widget/michaelpeter.near/Demo
+          </span>
+        </Link>
+        <span className="text-lg">
+          Static mockup of NEAR Social Welcome widget
         </span>
-      </Link>
-      <Link href="editor">
-        <span className="text-lg text-teal-400 underline">/editor</span>
-      </Link>
+      </div>
+      <div className="flex flex-col gap-1 items-center">
+        <Link href="editor">
+          <span className="text-lg text-teal-400 underline">/editor</span>
+        </Link>
+        <span className="text-lg">Loads source from above demo</span>
+      </div>
     </div>
   );
 }
