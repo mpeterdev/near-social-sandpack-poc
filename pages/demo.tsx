67469c87fe04d68d901d188a8401afff7fadd59d
Michael Peter <m.peter.dev@gmail.com>
2022-12-19 06:04:33 +0000

    fix build issue

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -74,7 +74,7 @@ function LinkOuts() {
       <h2 className="text-2xl">Get Involved</h2>
       <div className="flex flex-row gap-2">
         <button className="border rounded p-2 text-cyan-50 hover:bg-neutral-800">
-          What's Near Social?
+          What&apos;s Near Social?
         </button>
         <button className="border rounded p-2 text-cyan-50 hover:bg-neutral-800">
           Documentation
